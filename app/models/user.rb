812a49e30910e524e6abc16f0cfc4cf97c407cbb
midor030 <itigodaifuku030@icloud.com>
2020-11-17 19:26:10 +0900

    validates設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :familyname_kanji, presence: true
+  validates :firstname_kanji,  presence: true
+  validates :familyname_kana,  presence: true
+  validates :firstname_kana,   presence: true
+  validates :nickname,         presence: true
+  validates :birthday,         presence: true
 end
