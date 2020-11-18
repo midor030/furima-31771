07f4a12ab94c6029079033d1176b34cff8644974
midor030 <itigodaifuku030@icloud.com>
2020-11-18 16:11:09 +0900

    rspec導入

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :familyname_kanji, presence: true
-  validates :firstname_kanji,  presence: true
-  validates :familyname_kana,  presence: true
-  validates :firstname_kana,   presence: true
-  validates :nickname,         presence: true
-  validates :birthday,         presence: true
+  validates :firstname_kanji, presence: true
+  validates :familyname_kana, presence: true
+  validates :firstname_kana, presence: true
+  validates :nickname, presence: true
+  validates :birthday, presence: true
 end
