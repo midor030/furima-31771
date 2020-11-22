4c39e6e0ffbb2e67e23bdfaf983d0bac99f979fb
midor030 <itigodaifuku030@icloud.com>
2020-11-22 16:30:46 +0900

    アクティブハッシュアソシエーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,14 +2,21 @@ class Item < ApplicationRecord
   belongs_to :user
   has_many :orders
   has_one_attached :image
-  
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :state
+  belongs_to :load
+  belongs_to :area
+  belongs_to :days
+
   validates :item, presence: true
   validates :image, presence: true
   validates :explain, presence: true
-  validates :category_id, presence: true
-  validates :state_id, presence: true
-  validates :load_id, presence: true
-  validates :area_id, presence: true
-  validates :days_id, presence: true
+  validates :category_id, numericality: { other_than: 1 }, presence: true
+  validates :state_id, numericality: { other_than: 1 }, presence: true
+  validates :load_id, numericality: { other_than: 1 }, presence: true
+  validates :area_id, numericality: { other_than: 1 }, presence: true
+  validates :days_id, numericality: { other_than: 1 }, presence: true
   validates :price, presence: true
 end
