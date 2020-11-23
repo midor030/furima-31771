684df2a28ef0bd5349e8cef3b4d4029a1426599b
midor030 <itigodaifuku030@icloud.com>
2020-11-23 22:57:52 +0900

    console.log消し忘れ

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,7 +1,7 @@
 window.addEventListener('load', () => {
   const priceInput = document.getElementById("item-price");
   priceInput.addEventListener("input", () => {
-    const inputValue = priceInput.value;    console.log(inputValue);
+    const inputValue = priceInput.value;
     const addTaxDom = document.getElementById('add-tax-price');
     addTaxDom.innerHTML = Math.floor(inputValue / 10);
     const priceContent = document.getElementById('profit');
