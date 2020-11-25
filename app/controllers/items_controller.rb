db3ace178b89b7657945f088cb7808e3c6f59e87
midor030 <itigodaifuku030@icloud.com>
2020-11-25 19:39:42 +0900

    コントローラーbefore_action追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, except: [:index, :new, :create]
 
   def index
     @items = Item.all.order('item DESC')
@@ -19,15 +20,15 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
+    unless current_user.id == @item.user_id
+      redirect_to item_path
+    end
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item)
     else
@@ -40,4 +41,9 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:item, :explain, :image, :category_id, :state_id, :load_id, :area_id, :day_id, :price).merge(user_id: current_user.id)
   end
+
+  def  set_item
+    @item = Item.find(params[:id])
+  end
+
 end
