0e713d69f9acccb972390b2c28045271c172e412
midor030 <itigodaifuku030@icloud.com>
2020-12-04 16:23:47 +0900

    買った商品編集しようとすると戻る

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,7 +23,9 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    redirect_to item_path unless current_user.id == @item.user_id
+    if (current_user.id != @item.user.id) || (@item.order != nil)
+      redirect_to root_path
+    end
   end
 
   def update
@@ -50,4 +52,5 @@ class ItemsController < ApplicationController
   def set_item
     @item = Item.find(params[:id])
   end
+
 end
