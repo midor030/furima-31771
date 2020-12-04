1795dab667ad9384642b6157216f86135db6b136
midor030 <itigodaifuku030@icloud.com>
2020-12-04 14:15:00 +0900

    商品の表示順を修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,7 +3,7 @@ class ItemsController < ApplicationController
   before_action :set_item, except: [:index, :new, :create]
 
   def index
-    @items = Item.all.order('item DESC')
+    @items = Item.all.order('created_at DESC')
   end
 
   def new
