6aef450194923507c3d93774786aab07ee1fc1f8
midor030 <itigodaifuku030@icloud.com>
2020-12-04 14:53:26 +0900

    rubocop

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,7 +3,7 @@ class OrdersController < ApplicationController
   before_action :set_item
 
   def index
-    if (current_user.id == @item.user.id) || @item.order != nil
+    if (current_user.id == @item.user.id) || !@item.order.nil?
       redirect_to root_path
     else
       @user_item = UserItem.new
